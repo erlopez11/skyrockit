f0572157fb1be72b826d865cbf2dd41d1de13fb6
erlopez11 <lopezelainy@gmail.com>
2025-03-12 15:44:26 -0500

    wip: redirect to applications index and render template

--- controllers/applications.js
+++ controllers/applications.js
@@ -1,7 +1,18 @@
 const express = require('express');
 const router = express.Router();
-
 const User = require('../models/user.js');
+
+//GET /users/:userID/applications
+router.get('/', async (req, res) => {
+    try {
+        res.render('applications/index.ejs');
+    } catch (error) {
+        console.log(error);
+        res.redirect('/');
+    }
+});
+
+
 
 
 module.exports = router;
